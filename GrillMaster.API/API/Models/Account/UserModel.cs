b6cdea7129975b605bb5b9526c0e9c0e0f9e03c9
GarmusD <garmusd@gmail.com>
2022-07-25 19:11:00 +0200

    User login models

---
+++ GrillMaster.API/API/Models/Account/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GrillMaster.API.Models.Account
+{
+    public class UserModel
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+
+        // id, email, phone, etc...
+    }
+}
