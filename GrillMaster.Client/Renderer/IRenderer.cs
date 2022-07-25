081345af3403e301efea4a667f69accd81dd6ed4
GarmusD <garmusd@gmail.com>
2022-07-25 20:07:01 +0200

    Render to the selected output target

---
+++ GrillMaster.Client/Renderer/IRenderer.cs
@@ -0,0 +1,14 @@
+﻿using GrillMaster.Client.AppConfiguration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrillMaster.Client.Renderer
+{
+    internal interface IRenderer
+    {
+        void Render(OutputOptions outputOptions, string optimizedGrillJson);
+    }
+}
---
+++ GrillMaster.Client/Renderer/Renderer.cs
@@ -0,0 +1,27 @@
+﻿using GrillMaster.Client.AppConfiguration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrillMaster.Client.Renderer
+{
+    internal class Renderer
+    {
+        public static void RenderOutput(OutputOptions outputOptions, string optimizedOrder)
+        {
+            GetRenderer(outputOptions.Output).Render(outputOptions, optimizedOrder);
+        }
+
+        private static IRenderer GetRenderer(Output output)
+        {
+            return output switch
+            {
+                Output.Console => new Console.ConsoleRenderer(),
+                Output.Json => new Json.JsonRenderer(),
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+}
