a79c18387a5d79d67a0e1deadc8749b7d02c062b
GarmusD <garmusd@gmail.com>
2022-07-25 20:05:25 +0200

    Console renderer

---
+++ GrillMaster.Client/Renderer/Console/OptimizedItemEx.cs
@@ -0,0 +1,15 @@
+﻿using GrillMaster.Client.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrillMaster.Client.Renderer.Console
+{
+    internal class OptimizedItemEx
+    {
+        public OptimizedItem Item { get; init; }
+        public char FillChar { get; init; }
+    }
+}
