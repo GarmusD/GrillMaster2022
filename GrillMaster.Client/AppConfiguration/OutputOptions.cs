4a19249b4078ef519fcf77297472ed3222276175
GarmusD <garmusd@gmail.com>
2022-07-25 20:02:18 +0200

    Output cmd line options

---
+++ GrillMaster.Client/AppConfiguration/OutputOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrillMaster.Client.AppConfiguration
+{
+    public class OutputOptions
+    {
+        public Output Output { get; set; } = Output.Console;
+        public FileInfo? JsonOutputPath { get; set; } = null;
+    }
+}
