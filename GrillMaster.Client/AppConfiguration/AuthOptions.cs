176415c0be673f67ac70f4e5bf981ed07395a8ee
GarmusD <garmusd@gmail.com>
2022-07-25 20:01:59 +0200

    Autorization cmd line options

---
+++ GrillMaster.Client/AppConfiguration/AuthOptions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrillMaster.Client.AppConfiguration
+{
+    public class AuthOptions
+    {
+        public string UserName { get => _username; set => SetUserName(value); }
+        public string Password { get => _password; set => SetPassword(value); }
+
+        private string _username = ConfigDefaults.UserOptions.UserName;
+        private string _password = ConfigDefaults.UserOptions.Password;
+
+        private void SetUserName(string value)
+        {
+            if (string.IsNullOrEmpty(value) || value == ConfigDefaults.Placeholders.UserName) 
+                value = ConfigDefaults.UserOptions.UserName;
+
+            _username = value;
+        }
+
+        private void SetPassword(string value)
+        { 
+            if (string.IsNullOrEmpty(value) || value == ConfigDefaults.Placeholders.Password) 
+                value = ConfigDefaults.UserOptions.Password;
+
+            _password = value;
+        }
+    }
+}
