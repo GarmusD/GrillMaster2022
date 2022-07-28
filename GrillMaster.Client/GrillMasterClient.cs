1a91b551507325c77b92f23dcd5270fb144fe411
GarmusD <garmusd@gmail.com>
2022-07-28 23:13:21 +0200

    Now using HttpClient, RestSharp no more required

--- GrillMaster.Client/GrillMasterClient.cs
+++ GrillMaster.Client/GrillMasterClient.cs
@@ -1,50 +1,110 @@
-﻿using RestSharp;
-using GrillMaster.Client.AppConfiguration;
-using GrillMaster.Client.Exceptions;
-using RestSharp.Authenticators;
-using GrillMaster.Client.DTO;
+﻿using GrillMaster.Data.DTO;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace GrillMaster.Client
 {
-    internal class GrillMasterClient
+    public class GrillMasterClient : IDisposable
     {
-        private readonly AppConfig _appConfig;
-        private readonly RestClient _restClient;
+        private readonly HttpClient _httpClient;
 
-        public GrillMasterClient(AppConfig appConfig)
+        public GrillMasterClient(Uri baseUri)
         {
-            _appConfig = appConfig;
-            _restClient = new RestClient(_appConfig.Server.Uri);
-        }
-        
-        public string DoGrillOrder(string jsonOrder)
-        {
-            if (_restClient.Authenticator is null) Authenticate();
-
-            RestRequest request = new ("/api/grill/optimize", Method.Post);
-            request.AddBody(jsonOrder, "application/json");
-            RestResponse response = _restClient.Execute(request);
-            if (response.IsSuccessful && response.Content != null)
+            _httpClient = new()
             {
-                return response.Content;
+                BaseAddress = baseUri
+            };
+        }
+
+        /// <summary>
+        /// Authenticate to API server
+        /// </summary>
+        /// <param name="authUserRequest">Username and Password</param>
+        /// <returns></returns>
+        /// <exception cref="GrillMasterOfflineException"/>
+        /// <exception cref="GrillMasterNotAuthenticatedException"/>
+        public Task<string> AuthenticateAsync(AuthUserRequest authUserRequest)
+        {
+            return AuthenticateAsync(authUserRequest, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Authenticate to API server
+        /// </summary>
+        /// <param name="authUserRequest">Username and Password</param>
+        /// <param name="token">Cancellation token</param>
+        /// <returns></returns>
+        /// <exception cref="GrillMasterOfflineException"></exception>
+        /// <exception cref="GrillMasterNotAuthenticatedException"></exception>
+        public async Task<string> AuthenticateAsync(AuthUserRequest authUserRequest, CancellationToken token)
+        {            
+            var request = new HttpRequestMessage(HttpMethod.Post, "/api/user/login");
+            request.Headers.Add("Accept", "text/plain");
+            request.Content = JsonContent.Create(authUserRequest);
+
+            var response = await _httpClient.SendAsync(request, token);
+
+            if (response.IsSuccessStatusCode)
+            {
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await response.Content.ReadAsStringAsync(token));
+                return await response.Content.ReadAsStringAsync(token);
             }
+            else if (response.StatusCode == 0)
+                throw new GrillMasterOfflineException();
+
+            throw new GrillMasterNotAuthenticatedException();
+        }
+
+        /// <summary>
+        /// Optimize grill order.
+        /// </summary>
+        /// <param name="grillOrder"></param>
+        /// <returns>Returns optimized grill order.</returns>
+        /// <exception cref="GrillMasterInvalidJsonException"/>
+        /// <exception cref="GrillMasterApiErrorException"/>
+        /// <exception cref="GrillMasterOfflineException"/>
+        public Task<OptimizedOrder> OptimizeGrillOrderAsync(GrillOrder grillOrder)
+        {
+            return OptimizeGrillOrderAsync(grillOrder, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Optimize grill order.
+        /// </summary>
+        /// <param name="grillOrder"></param>
+        /// <returns>Returns optimized grill order.</returns>
+        /// <exception cref="GrillMasterInvalidJsonException"/>
+        /// <exception cref="GrillMasterApiErrorException"/>
+        /// <exception cref="GrillMasterOfflineException"/>
+        public async Task<OptimizedOrder> OptimizeGrillOrderAsync(GrillOrder grillOrder, CancellationToken token)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, "/api/grill/optimize");
+            request.Headers.Add("Accept", "application/json");
+            request.Headers.Add("Accept", "'application/problem+json'");
+            request.Content = JsonContent.Create(grillOrder);
+
+            var response = await _httpClient.SendAsync(request, token);
+
+            if (response.IsSuccessStatusCode)
+            {
+                try
+                {
+                    var responseStr = await response.Content.ReadAsStringAsync(token);
+                    OptimizedOrder? optimizedOrder = JsonSerializer.Deserialize<OptimizedOrder>(responseStr, new JsonSerializerOptions(JsonSerializerDefaults.Web));
+                    if (optimizedOrder != null) return optimizedOrder;
+                }
+                catch(Exception ex) when (ex is JsonException || ex is NotSupportedException)
+                {
+                    throw new GrillMasterInvalidJsonException();
+                }
+            }
+            else if(response.StatusCode == 0)
+                throw new GrillMasterOfflineException();
+
             throw new GrillMasterApiErrorException();
         }
 
-        private void Authenticate()
-        {
-            RestRequest request = new ("/api/user/login", Method.Post);
-            request.AddJsonBody(new AuthUserRequest(_appConfig.Auth.UserName, _appConfig.Auth.Password));
-            RestResponse response = _restClient.Execute(request);
-            if (response.StatusCode == 0) throw new GrillMasterOfflineException();
-            if(response.IsSuccessful && response.Content != null)
-            {
-                // Token string in response.Content is received encapsulated within quotes "__TOKEN__"
-                // Workaround - remove quotes.
-                _restClient.Authenticator = new JwtAuthenticator(response.Content.Replace("\"", ""));
-                return;
-            }
-            throw new GrillMasterNotAuthenticatedException();
-        }
+        public void Dispose() => _httpClient.Dispose();
     }
 }
